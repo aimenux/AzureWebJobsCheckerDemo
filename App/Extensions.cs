9c6441cc189090c2573e2629f2fc6b0145a54a78
Aymen TROUDI <Aymen.TROUDI@sodexo.com>
2023-04-05 16:48:43 +0200

    Upgrade to .net 6.0

---
+++ App/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace App;
+
+public static class Extensions
+{
+    public static void AddNonGenericLogger(this ILoggingBuilder loggingBuilder)
+    {
+        var services = loggingBuilder.Services;
+        services.AddSingleton(serviceProvider =>
+        {
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            return loggerFactory.CreateLogger("WebJobsCheckerDemo");
+        });
+    }
+    
+    public static void AddJsonFile(this IConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.SetBasePath(GetDirectoryPath());
+        var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+        configurationBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        configurationBuilder.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
+    }
+
+    public static void AddUserSecrets(this IConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.AddUserSecrets(typeof(Program).Assembly);
+    }
+    
+    private static string GetDirectoryPath()
+    {
+        try
+        {
+            return Path.GetDirectoryName(GetAssemblyLocation());
+        }
+        catch
+        {
+            return Directory.GetCurrentDirectory();
+        }
+    }
+    
+    private static string GetAssemblyLocation() => Assembly.GetExecutingAssembly().Location;
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using Lib.Configuration;
 using Lib.Extensions;
@@ -8,73 +7,55 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Options;
 
-namespace App
+namespace App;
+
+public static class Program
 {
-    public static class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        using var host = CreateHostBuilder(args).Build();
+        var settings = host.Services.GetService<IOptions<Settings>>().Value;
+        var webjobInfoProvider = host.Services.GetService<IWebJobInfoProvider>();
+
+        foreach (var webjob in settings.WebJobs)
         {
-            using var host = CreateHostBuilder(args).Build();
-            var settings = host.Services.GetService<IOptions<Settings>>().Value;
-            var webjobInfoProvider = host.Services.GetService<IWebJobInfoProvider>();
-
-            foreach (var webjob in settings.WebJobs)
-            {
-                ConsoleColor.Yellow.WriteLine($"Checking infos for webjob '{webjob}'");
-                var webjobInfo = await webjobInfoProvider.GetWebJobInfoAsync(webjob);
-                ConsoleColor.Green.WriteLine(webjobInfo);
-            }
-
-            ConsoleColor.Gray.WriteLine("Press any key to exit !");
-            Console.ReadKey();
+            ConsoleColor.Yellow.WriteLine($"Checking infos for webjob '{webjob}'");
+            var webjobInfo = await webjobInfoProvider.GetWebJobInfoAsync(webjob);
+            ConsoleColor.Green.WriteLine(webjobInfo);
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureHostConfiguration(config =>
-                {
-                    config.AddCommandLine(args);
-                    config.AddEnvironmentVariables();
-                    config.SetBasePath(Directory.GetCurrentDirectory());
-                })
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    config.AddCommandLine(args);
-                    config.AddEnvironmentVariables();
-                    config.SetBasePath(Directory.GetCurrentDirectory());
-                    var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
-                    config.AddJsonFile("appsettings.json", false, true);
-                    config.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
-                })
-                .ConfigureLogging((context, builder) =>
-                {
-                    builder.AddNonGenericLogger();
-                    builder.AddConsole(options =>
-                    {
-                        options.DisableColors = false;
-                        options.TimestampFormat = "[HH:mm:ss:fff] ";
-                    });
-                    builder.AddFilter("System.Net.Http", LogLevel.Warning);
-                    builder.AddFilter("Microsoft.Extensions.Http", LogLevel.Warning);
-                    builder.AddConfiguration(context.Configuration.GetSection("Logging"));
-                })
-                .ConfigureServices((context, services) =>
-                {
-                    services.Configure<Settings>(context.Configuration.GetSection(nameof(Settings)));
-                    services.AddTransient<IWebJobInfoProvider, WebJobInfoProvider>();
-                    services.AddHttpClient();
-                });
-
-        private static void AddNonGenericLogger(this ILoggingBuilder loggingBuilder)
-        {
-            var services = loggingBuilder.Services;
-            services.AddSingleton(serviceProvider =>
-            {
-                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-                return loggerFactory.CreateLogger("WebJobsCheckerDemo");
-            });
-        }
+        ConsoleColor.Gray.WriteLine("Press any key to exit !");
+        Console.ReadKey();
     }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile();
+                config.AddUserSecrets();
+                config.AddEnvironmentVariables();
+                config.AddCommandLine(args);
+            })
+            .ConfigureLogging((context, builder) =>
+            {
+                builder.AddNonGenericLogger();
+                builder.AddSimpleConsole(options =>
+                {
+                    options.TimestampFormat = "[HH:mm:ss:fff] ";
+                    options.ColorBehavior = LoggerColorBehavior.Disabled;
+                });
+                builder.AddFilter("System.Net.Http", LogLevel.Warning);
+                builder.AddFilter("Microsoft.Extensions.Http", LogLevel.Warning);
+                builder.AddConfiguration(context.Configuration.GetSection("Logging"));
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.Configure<Settings>(context.Configuration.GetSection(nameof(Settings)));
+                services.AddTransient<IWebJobInfoProvider, WebJobInfoProvider>();
+                services.AddHttpClient();
+            });
 }
